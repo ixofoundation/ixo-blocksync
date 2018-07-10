9f44031ba315ee6bdc681d76297dccf335a167b1
Nicolaas Vercuiel <nicolaas.vercuiel73@gmail.com>
2018-07-10 14:43:03 +0200

    # Added docker files for aws deployment.

---
+++ bin/start.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#git clone https://github.com/ixofoundation/ixo-block-sync.git
+#cd ixo-block-sync
+#green=`tput setaf 2`
+echo "***********************************"
+echo "* IXO BLOCK SYNC                  *"
+echo "***********************************"
+echo ""
+echo "Build IXO Block Sync"
+CURRENT_DIR=`dirname $0`
+ROOT_DIR=$CURRENT_DIR/..
+
+$ROOT_DIR/node_modules/typescript/bin/tsc 
+docker build -t trustlab/ixo-block-sync $ROOT_DIR
+
+docker-compose up --no-start
+# docker-compose create
+docker-compose start db
+
+# attempting to wait for mongodb to be ready
+$ROOT_DIR/bin/wait-for-service.sh db 'waiting for connections on port' 10
+docker-compose start app
+
+echo -n "Starting IXO Block Sync ..."
+sleep 5
+echo ${green} "done"
+docker-compose logs --tail 13 app
+echo ""
+echo "***********************************"
+echo "* IXO BLOCK SYNC COMPLETE          *"
+echo "***********************************"
+docker-compose ps
