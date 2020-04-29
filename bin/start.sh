2896c8e192d0eed21f4b9ac92187281ab55fed93
migueldingli1997 <migueldingli97@gmail.com>
2020-04-29 11:40:36 +0200

    Updated start.sh

--- bin/start.sh
+++ bin/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#git clone https://github.com/ixofoundation/ixo-block-sync.git
-#cd ixo-block-sync
+#git clone https://github.com/ixofoundation/ixo-blocksync.git
+#cd ixo-blocksync
 #green=`tput setaf 2`
 echo "***********************************"
 echo "* IXO BLOCK SYNC                  *"
@@ -10,8 +10,8 @@ echo "Build IXO Block Sync"
 CURRENT_DIR=`dirname $0`
 ROOT_DIR=$CURRENT_DIR/..
 
-$ROOT_DIR/node_modules/typescript/bin/tsc 
-docker build -t trustlab/ixo-block-sync $ROOT_DIR
+$ROOT_DIR/node_modules/typescript/bin/tsc
+docker build -t trustlab/ixo-blocksync $ROOT_DIR
 
 docker-compose up --no-start
 # docker-compose create
@@ -19,12 +19,12 @@ docker-compose start block-sync-db
 
 # attempting to wait for mongodb to be ready
 $ROOT_DIR/bin/wait-for-service.sh block-sync-db 'waiting for connections on port' 10
-docker-compose start ixo-block-sync
+docker-compose start ixo-blocksync
 
 echo -n "Starting IXO Block Sync ..."
 sleep 5
 echo ${green} "done"
-docker-compose logs --tail 13 ixo-block-sync
+docker-compose logs --tail 13 ixo-blocksync
 echo ""
 echo "***********************************"
 echo "* IXO BLOCK SYNC COMPLETE          *"
