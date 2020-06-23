0fbedbab2c7bad7583c725f8b3c74727f2dd97cd
migueldingli1997 <migueldingli97@gmail.com>
2020-06-23 14:09:17 +0200

    Switched sign_data to POST

--- src/handlers/auth_handler.ts
+++ src/handlers/auth_handler.ts
@@ -4,7 +4,7 @@ export class AuthHandler {
   getSignData = (msgHex: string) => {
     return new Promise((resolve: Function, reject: Function) => {
       let rest = (process.env.BC_REST || 'localhost:1317');
-      axios.get(rest + '/sign_data/' + msgHex)
+      axios.post(rest + '/sign_data', {msg: msgHex})
         .then((response) => {
           if (response.status == 200) {
             resolve(response.data);
