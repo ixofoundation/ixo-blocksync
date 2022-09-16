02f5f3debbe7982b1548d66949a47436cc589fec
LJS243 <lohanjs@outlook.com>
2022-09-16 08:46:54 +0200

    fix: type conversions for CosmosBlock

--- src/handlers/cosmos_handler.ts
+++ src/handlers/cosmos_handler.ts
@@ -1,18 +1,24 @@
 import { prisma } from "../prisma/prisma_client";
 
-export const createCosmosBlock = async (blockHash: string, block: any, blockResult: any) => {
+export const createCosmosBlock = async (
+    blockHash: string,
+    block: any,
+    blockResult: any,
+) => {
     try {
         let total_gas = 0;
-        blockResult.txs_results.forEach((tx: any) => {
-            total_gas += tx.gas_used;
-        });
+        if (blockResult.txs_results) {
+            blockResult.txs_results.forEach((tx: any) => {
+                total_gas += parseInt(tx.gas_used);
+            });
+        }
         const blockDoc = {
-            height: block.header.height,
+            height: parseInt(block.header.height),
             hash: blockHash,
             num_txs: block.data.txs.length,
             total_gas: total_gas,
             proposer_address: block.header.proposer_address,
-            timestamp: new Date(Date.parse(block.block.header.time)),
+            timestamp: new Date(Date.parse(block.header.time)),
         };
         const res = await prisma.cosmosBlock.create({ data: blockDoc });
         return res;
