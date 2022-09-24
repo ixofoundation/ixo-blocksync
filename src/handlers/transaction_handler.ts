832b0f09a7096d4a53c509987813cefee164bdfc
LJS243 <lohanjs@outlook.com>
2022-09-24 10:21:22 +0200

    fix: transaction api endpoints

--- src/handlers/transaction_handler.ts
+++ src/handlers/transaction_handler.ts
@@ -1,30 +1,33 @@
 import { prisma } from "../prisma/prisma_client";
 
-export const listBlockTransactions = async (filter?: any) => {
-    let res: any;
-    if (filter) {
+export const listBlockTransactions = async (filter: any) => {
+    try {
         if (filter.hasOwnProperty("type") && filter.hasOwnProperty("address")) {
-            res = await prisma.transaction.findMany({
+            const res = await prisma.transaction.findMany({
                 where: {
                     type: filter.type,
                     from: filter.address,
                 },
             });
+            return res;
         } else if (filter.hasOwnProperty("type")) {
-            res = await prisma.transaction.findMany({
+            const res = await prisma.transaction.findMany({
                 where: {
                     type: filter.type,
                 },
             });
+            return res;
         } else if (filter.hasOwnProperty("address")) {
-            res = await prisma.transaction.findMany({
+            const res = await prisma.transaction.findMany({
                 where: {
                     from: filter.address,
                 },
             });
+            return res;
         }
-    } else {
-        res = await prisma.transaction.findMany({ take: -10 });
+    } catch (error) {
+        console.log(error);
+        return;
     }
-    return res;
+    return;
 };
