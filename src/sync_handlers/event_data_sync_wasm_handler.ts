b50001d3df8f0d74ac08cc7855ff295d71da5cde
Michael Pretorius <michael@ixo.world>
2023-09-04 10:04:50 +0200

    fix: permanent fix for wasm first events

--- src/sync_handlers/event_data_sync_wasm_handler.ts
+++ src/sync_handlers/event_data_sync_wasm_handler.ts
@@ -3,7 +3,7 @@ import { getWasmAttr, splitAttributesByKeyValue } from "../util/helpers";
 import { ENTITY_MODULE_CONTRACT_ADDRESS } from "../util/secrets";
 import { GetEventType } from "../types/getBlock";
 
-export const syncWasmEventData = async (event: GetEventType) => {
+export const syncWasmEventData = async (event: GetEventType): Promise<any> => {
   try {
     const contractAddress = getWasmAttr(event.attributes, "_contract_address");
     const action = getWasmAttr(event.attributes, "action");
@@ -13,13 +13,18 @@ export const syncWasmEventData = async (event: GetEventType) => {
       const tokenId = getWasmAttr(event.attributes, "token_id");
 
       if (action === "mint") {
-        // use setTimeout to wait for the entity to be created as creation event is after wasm event
-        setTimeout(async () => {
-          await prisma.entity.update({
-            where: { id: tokenId },
-            data: { owner: getWasmAttr(event.attributes, "owner") },
-          });
-        }, 500);
+        // if action for entity contract address is mint it means it is a nft minting and since wasm events come before
+        // module events it means the entity creation event on entity module didnt happen yet so we need to delay this
+        // function until the entity creation event happens, which is after iid creation event, thus the skip 2
+        return {
+          skip: 2,
+          func: async () => {
+            await prisma.entity.update({
+              where: { id: tokenId },
+              data: { owner: getWasmAttr(event.attributes, "owner") },
+            });
+          },
+        };
       } else if (action === "transfer_nft") {
         await prisma.entity.update({
           where: { id: tokenId },
@@ -37,23 +42,29 @@ export const syncWasmEventData = async (event: GetEventType) => {
       // split attributes by action as cosmwasm joins all attributes into one array
       const messages = splitAttributesByKeyValue(event.attributes as any);
       for (const message of messages) {
-        setTimeout(
-          async () => {
-            await prisma.token.update({
-              where: { id: getWasmAttr(message, "token_id") },
-              data: {
-                tokenTransaction: {
-                  create: {
-                    from: getWasmAttr(message, "from"),
-                    to: getWasmAttr(message, "to"),
-                    amount: getWasmAttr(message, "amount"),
-                  },
-                },
-              },
-            });
-          },
-          getWasmAttr(message, "from") ? 0 : 500
-        );
+        const tokenTransaction = {
+          from: getWasmAttr(message, "from"),
+          to: getWasmAttr(message, "to"),
+          amount: getWasmAttr(message, "amount"),
+        };
+        if (getWasmAttr(message, "from")) {
+          await prisma.token.update({
+            where: { id: getWasmAttr(message, "token_id") },
+            data: { tokenTransaction: { create: tokenTransaction } },
+          });
+        } else {
+          // if no from it means it is a token minting and since wasm events come before module events it means the token creation
+          // event on token module didnt happen yet so we need to delay this function until the token creation event happens
+          return {
+            skip: 1,
+            func: async () => {
+              await prisma.token.update({
+                where: { id: getWasmAttr(message, "token_id") },
+                data: { tokenTransaction: { create: tokenTransaction } },
+              });
+            },
+          };
+        }
       }
     }
   } catch (error) {
--- src/sync_handlers/event_sync_handler.ts
+++ src/sync_handlers/event_sync_handler.ts
@@ -7,6 +7,7 @@ export const syncEvents = async (
   blockHeight: number,
   timestamp: Date
 ) => {
+  let delayedFunction: any = null;
   let i = 0;
   for (const event of events) {
     if (i === 0) {
@@ -14,11 +15,32 @@ export const syncEvents = async (
       i++;
     }
 
+    let res: any = null;
     try {
-      if (event.type === "wasm") await syncWasmEventData(event);
-      else await syncEventData(event, blockHeight, timestamp);
+      if (event.type === "wasm") {
+        res = await syncWasmEventData(event);
+      } else {
+        await syncEventData(event, blockHeight, timestamp);
+      }
+      // run delayed function after syncEventData if skip value 1
+      if (
+        delayedFunction &&
+        delayedFunction.skip === 1 &&
+        delayedFunction.func instanceof Function
+      ) {
+        await delayedFunction.func();
+      }
     } catch (error) {
       console.error("syncEvent: ", error.message);
+    } finally {
+      // if delayedFunction is not null and skip value is not 1, then minus current skip value by 1
+      // NOTE: this means if there is current delayedFunction, then a new one cant be set until the current one is run
+      // which theoretically should never happen
+      if (delayedFunction && delayedFunction.skip !== 1) {
+        delayedFunction.skip--;
+      } else {
+        delayedFunction = res;
+      }
     }
   }
 };
