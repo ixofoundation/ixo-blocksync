f123da835d5ecf5c8d28db2fffc376e736be0f1d
Michael Pretorius <michael@ixo.world>
2024-04-06 09:47:13 +0200

    feat: add programatic prisma migrations

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,21 @@ require("dotenv").config();
 import { app } from "./app";
 import http from "http";
 import * as SyncBlocks from "./sync/sync_blocks";
-import { PORT } from "./util/secrets";
+import { DATABASE_URL, PORT, MIGRATE_DB_PROGRAMATICALLY } from "./util/secrets";
 import * as SyncChain from "./sync/sync_chain";
 import * as SyncBlocksCustom from "./sync/sync_custom";
+import { prismaMigrate } from "./prisma/migration";
 
-SyncChain.syncChain().then(() => SyncBlocks.startSync());
+(async () => {
+  // first apply db migrations if env var set, for prod dbs where no access to shell
+  if (MIGRATE_DB_PROGRAMATICALLY) {
+    console.log("MIGRATE_DB_PROGRAMATICALLY: ", MIGRATE_DB_PROGRAMATICALLY);
+    await prismaMigrate(DATABASE_URL || "");
+  }
 
-const server = http.createServer(app);
-server.listen(PORT, () => console.log(`Listening on ${PORT}`));
+  // server setup and start logic
+  SyncChain.syncChain().then(() => SyncBlocks.startSync());
+
+  const server = http.createServer(app);
+  server.listen(PORT, () => console.log(`Listening on ${PORT}`));
+})();
--- src/util/secrets.ts
+++ src/util/secrets.ts
@@ -3,6 +3,8 @@ export const SENTRYDSN = process.env.SENTRYDSN || undefined;
 export const RPC = process.env.RPC || "http://localhost:26657";
 export const DATABASE_URL = process.env.DATABASE_URL;
 export const DATABASE_URL_CORE = process.env.DATABASE_URL_CORE;
+export const MIGRATE_DB_PROGRAMATICALLY =
+  Number(process.env.MIGRATE_DB_PROGRAMATICALLY ?? "0") || 0;
 export const TRUST_PROXY = process.env.TRUST_PROXY || 1;
 export const ENTITY_MODULE_CONTRACT_ADDRESS =
   process.env.ENTITY_MODULE_CONTRACT_ADDRESS || "";
